1afb5e163b829e55bc6b2440b76d750179fcc706
Ronnie <ronaldbutironi@gmail.com>
2020-02-24 16:42:04 -0500

    Added Bulma started customizin my project thumbsup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <MyNav></MyNav>
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import  MyNav  from "@/components/Nav.vue";
+export default {
+  components: {
+    MyNav
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
